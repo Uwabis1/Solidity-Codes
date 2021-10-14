93a74b8ed8f4f4497f180e6d8511c8f8d0c484ff
Uwabis <90389031+Uwabis1@users.noreply.github.com>
2021-10-14 08:09:00 +0100

    Add files via upload

---
+++ mappingIterator.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.6.12;
+
+contract Mapping {
+    mapping(address => uint8) public feedback;
+    address[] public addresses; //doing an arrray of the keys helps you loop through the addresses
+    function giveFeedback(uint8 _feedback) public {
+        feedback[msg.sender] = _feedback;
+        addresses.push(msg.sender);
+    }
+    function count() public view returns(uint) {
+        return addresses.length;
+    }
+    function returnAddresses() public view returns(address[] memory ) {
+        return addresses;
+    }
+}
+
