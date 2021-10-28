4ce452da566ce79c1dfb5e8f70e4403b11c6486a
Uwabis <90389031+Uwabis1@users.noreply.github.com>
2021-10-28 14:17:08 +0100

    Update mappingIterator.sol

--- mappingIterator.sol
+++ mappingIterator.sol
@@ -2,14 +2,18 @@ pragma solidity 0.6.12;
 
 contract Mapping {
     mapping(address => uint8) public feedback;
-    address[] public addresses; //doing an arrray of the keys helps you loop through the addresses
+    
+    address[] public addresses; //By declaring an arrray of the keys helps you loop through the addresses
+    
     function giveFeedback(uint8 _feedback) public {
         feedback[msg.sender] = _feedback;
         addresses.push(msg.sender);
     }
+    
     function count() public view returns(uint) {
         return addresses.length;
     }
+    
     function returnAddresses() public view returns(address[] memory ) {
         return addresses;
     }
