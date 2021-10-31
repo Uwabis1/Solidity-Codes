cb05507a44e5c829bc78eb5c34fe8f8c729fad77
Uwabis <90389031+Uwabis1@users.noreply.github.com>
2021-10-31 14:25:21 +0100

    Update withdrawalPattern.sol

--- withdrawalPattern.sol
+++ withdrawalPattern.sol
@@ -1,13 +1,17 @@
 pragma solidity 0.6.12;
 
+
 contract Richest {
     address payable richest;
     uint mostSent;
+    
     mapping(address => uint) public investors;
+    
     constructor () public payable {
-        richest = msg.sender; //why i am doing this directly? so that when the contract is deployed, i (as the first person) would be d rishest
+        richest = msg.sender; //why i am doing this directly? so that when the contract is deployed, i (as the first person) would be d rishest! The contract holder
         mostSent = msg.value;
-        richest.transfer(msg.value); //this returns the money back to the person. i dont want to store the money in the contract, just want to make sure they are the rishest. 
+        richest.transfer(msg.value); //this returns the money back to the person. i dont want to store the money in the contract, 
+        //just want to make sure they are the rishest. 
     }
     function invest() payable public {
         require(msg.value > mostSent,"Not enough money to become richest");
@@ -16,12 +20,14 @@ contract Richest {
         // richest.transfer(msg.value); - dont transfer within the function itself.
         investors[msg.sender] = msg.value;
     }
+    
     function withdraw() public {
         uint amount = investors[msg.sender];
         investors[msg.sender] = 0;
         msg.sender.transfer(amount);
     }
 }
+
 contract B {
     fallback () payable external {
         revert();
