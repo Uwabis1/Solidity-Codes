da316a8f7999156cb8459a091c7e3b32a5aaac15
Uwabis <90389031+Uwabis1@users.noreply.github.com>
2021-11-08 13:05:24 +0100

    Update factoryPattern.sol

--- factoryPattern.sol
+++ factoryPattern.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.6.12;
+
 // factory patterns helps you always have records of the contracts that have been deployed from a parent contract. so that you dont lose 
 // the tracability of any of the sub contracts. e.g; you can always retrive them from the carsManufactured, in the below psuedo case. 
 
@@ -9,6 +10,7 @@ contract Toyota {
         carsManufactured.push(new Car());
     }
 }
+
 
 contract Car {
     
